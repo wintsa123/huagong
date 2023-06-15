c23249a167a3f172ad5570cdb0e557c4b3e5861d
wintsa <wintsa123@gmail.com>
2023-06-15 09:59:08 +0800

    0615

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import 'tdesign-vue-next/es/style/index.css';
+import TDesign from 'tdesign-vue-next';
+// import './assets/rest.css';
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(TDesign);
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,54 @@
+<template>
+    <t-head-menu default-value="item1" v-model="menu1Value" theme="light" @change="changeHandler">
+        <template #logo>
+            <img src="https://www.tencent.com/img/index/menu_logo_hover.png" width="136" />
+        </template>
+        <t-menu-item value="item1"> 首页 </t-menu-item>
+        <t-menu-item value="item2"> 已选内容 </t-menu-item>
+        <t-menu-item value="item3"> 菜单内容二 </t-menu-item>
+        <t-menu-item value="item4"> 菜单内容二 </t-menu-item>
+
+        <t-menu-item value="item5"> 菜单内容三 </t-menu-item>
+        <template #operations>
+            <div >
+
+                <t-button variant="text" shape="square">
+                    <template #icon><t-icon name="mail" /></template>
+                </t-button>
+
+            </div>
+        </template>
+    </t-head-menu>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const menu1Value = ref('item2');
+
+const changeHandler = (active) => {
+  console.log('change', active);
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
+<style lang="less" scoped>
+.t-menu__operations {
+    .t-button {
+        margin-left: 8px;
+    }
+}
+
+.t-demo-menu--dark {
+    .t-button {
+        color: #fff;
+
+        &:hover {
+            background-color: #4b4b4b;
+            border-color: transparent;
+            --ripple-color: #383838;
+        }
+    }
+}
+</style>
+  
